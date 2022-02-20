7b2b23671fa77d53b09c45b53cc76846bb910af2
thakshilad <d.thakshila@mubasher.net>
2022-02-21 00:31:56 +0530

    project structure

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'appflow-test',
+  webDir: 'dist/appflow-test',
+  bundledWebRuntime: false
+};
+
+export default config;
